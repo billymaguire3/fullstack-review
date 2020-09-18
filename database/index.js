f0a8468b516a7030571a4c16db08b8a301277afc
William Maguire <billymaguire3@gmail.com>
2020-09-18 16:27:44 -0400

    Evironment set up and add schema to database index file

--- database/index.js
+++ database/index.js
@@ -1,16 +1,54 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
-let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
-});
+const db = mongoose.connection;
 
-let Repo = mongoose.model('Repo', repoSchema);
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Successful connection!')
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
-}
+  let repoSchema = mongoose.Schema({
+    id: Number,
+    full_name: String,
+    description: String,
+    forks: Number,
+    language: String,
+    owner_id: Number,
+    owner_url: String
+    owner_login: String,
+    owner_avatarUrl: String,
+  });
+
+
+  let Repo = mongoose.model('Repo', repoSchema);
+
+  const marcus = new Repo({
+    id: 14563,
+    full_name: "marcus/git-this",
+    description: "Lecture on the fundamentals of keyword this",
+    forks: 45,
+    language: "javascript",
+    owner_id: 5683,
+    owner_url: "https://api.github.com/users/marcus",
+    owner_login: "marcus",
+    owner_avatarUrl: "https://avatars0.githubusercontent.com/u/582331?v=3"
+  })
+
+})
+  // let save = (err, repos) => {
+  //   // TODO: Your code here
+  //   // This function should save a repo or repos to
+  //   // the MongoDB
+  //   if (err) {
+  //     console.log(err);
+  //   }
+  // }
+
+  marcus.save = (err, marcus) => {
+    if (err) {console.log(err);}
+    else {
+      console.log('Successfully saved marcus repo: ', marcus);
+    }
+  }
 
 module.exports.save = save;
